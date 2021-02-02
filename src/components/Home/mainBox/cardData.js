ced7464c305115ea19a1a0080e6c17288e1ce600
develad <develadb@gmail.com>
2021-02-02 13:52:56 +0200

    figma add

--- src/components/Home/mainBox/cardData.js
+++ src/components/Home/mainBox/cardData.js
@@ -1,79 +1,78 @@
-export const info=[
-    {
-    mainTitle:"Frontend",    
-    infoPairs:[
-        {
-            title:"React",
-            src:"/images/react.png"
-        },
-        {   
-            title:"HTML5",
-            src:"/images/html-5.png"
-        },
-        {
-            title:"CSS3",
-            src:"/images/css3.png"
-        },
-        {
-            title:"JavaScript",
-            src:"/images/javascript.png"
-        },
-        {
-            title:"Sass",
-            src:"/images/sass.png"
-        },
-        {
-            title:"Bootstrap",
-            src:"/images/bootstrap.png"
-        },
-    ]
-},
-    {
-        mainTitle:"Backend",
-        infoPairs:[
-        {
-            title:"Node.js",
-            src:"/images/nodejs.png"
-        },
-        {   
-            title:"Express",
-            src:"/images/express.png"
-        },
-        {
-            title:"MongoDB",
-            src:"/images/mongodb.png"
-        },
-        {   
-            title:"npm",
-            src:"/images/npm.png"
-        },
-    ]
-},
-    {
-        mainTitle:"Deployment",
-        infoPairs:[
-        {
-            title:"Netlify",
-            src:"/images/netlify.png"
-        },
-        {   
-            title:"Heroku",
-            src:"/images/heroku.png"
-        },
-    ]
-},
-    {
-        mainTitle:"Version Control",
-        infoPairs:[
-        {
-            title:"Git",
-            src:"/images/git.png"
-        },
-        {   
-            title:"Github",
-            src:"/images/github.png"
-        },
-    ]
-},
-
-]
+export const info = [
+  {
+    mainTitle: 'Frontend',
+    infoPairs: [
+      {
+        title: 'React',
+        src: '/images/react.png',
+      },
+      {
+        title: 'HTML5',
+        src: '/images/html-5.png',
+      },
+      {
+        title: 'CSS3',
+        src: '/images/css3.png',
+      },
+      {
+        title: 'JavaScript',
+        src: '/images/javascript.png',
+      },
+      {
+        title: 'Bootstrap',
+        src: '/images/bootstrap.png',
+      },
+      {
+        title: 'Figma',
+        src: '/images/figma.png',
+      },
+    ],
+  },
+  {
+    mainTitle: 'Backend',
+    infoPairs: [
+      {
+        title: 'Node.js',
+        src: '/images/nodejs.png',
+      },
+      {
+        title: 'Express',
+        src: '/images/express.png',
+      },
+      {
+        title: 'MongoDB',
+        src: '/images/mongodb.png',
+      },
+      {
+        title: 'npm',
+        src: '/images/npm.png',
+      },
+    ],
+  },
+  {
+    mainTitle: 'Deployment',
+    infoPairs: [
+      {
+        title: 'Netlify',
+        src: '/images/netlify.png',
+      },
+      {
+        title: 'Heroku',
+        src: '/images/heroku.png',
+      },
+    ],
+  },
+  {
+    mainTitle: 'Version Control',
+    infoPairs: [
+      {
+        title: 'Git',
+        src: '/images/git.png',
+      },
+      {
+        title: 'Github',
+        src: '/images/github.png',
+      },
+    ],
+  },
+];
