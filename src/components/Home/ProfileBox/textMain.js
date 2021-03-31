8922326c8bdfdacfd723335c3a69085b780173d9
develad <develadb@gmail.com>
2021-03-31 15:16:25 +0300

    Fixing TextMain.js

--- src/components/Home/ProfileBox/textMain.js
+++ src/components/Home/ProfileBox/textMain.js
@@ -1,14 +1,7 @@
-// export const textMain=[
-//     "\"My name is Elad and i'm looking for my first Web Development job as a Junior Developer programmer\"",
+export const textMain = [
+  'My name is Elad and I am looking for a job as a Junior Developer',
 
-//     "\"I am a Self autodidact enthusiastic with big ambitions\"",
-    
-//     "\"I have great communication and social skills\"",
-// ]
-export const textMain=[
-    "My name is Elad and I am looking for a job as a Junior Developer",
+  'I am a self taught and an enthusiastic with great ambitions',
 
-    "I am a self taught and an enthusiastic with great ambitions",
-    
-    "I believe that communication and social skills working together in a group can have big contribution to the success of the development process",
-]
+  'I believe that communication and social skills working together in a group can have big contribution to the success of the development process',
+];
