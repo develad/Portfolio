d13e691c000909f40eaa9a57897e0b46d058e429
develad <develadb@gmail.com>
2021-04-02 01:43:29 +0300

    :tada: adding full stack weather app

--- src/components/Home/mainBox/Card.jsx
+++ src/components/Home/mainBox/Card.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
-function Card({infoItem}) {
-    // console.log(infoItem)
-    return (
-        <InnerBox>
-            <h3>{infoItem.title}</h3>
-            <Pic src={infoItem.src} alt={infoItem.title} />
-        </InnerBox>
-    )
+function Card({ infoItem }) {
+  // console.log(infoItem)
+  useEffect(() => {
+    fetch('https://dagbareshet.herokuapp.com');
+  }, []);
+  return (
+    <InnerBox>
+      <h3>{infoItem.title}</h3>
+      <Pic src={infoItem.src} alt={infoItem.title} />
+    </InnerBox>
+  );
 }
 
-export default Card
+export default Card;
 
 const InnerBox = styled.div`
-width:12rem;
-margin: 5px auto;
-display:flex;
-align-items:center;
-justify-content:space-between;
+  width: 12rem;
+  margin: 5px auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 const Pic = styled.img`
-height:30px;
-width:30px;
+  height: 30px;
+  width: 30px;
 `;
-
-
