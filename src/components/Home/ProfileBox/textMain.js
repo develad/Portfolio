25e8ccff38262318fcbf92e1a7accd71cdcdb474
develad <70572668+develad@users.noreply.github.com>
2022-05-21 15:33:50 +0300

    🎉 change text in textMain.js

--- src/components/Home/ProfileBox/textMain.js
+++ src/components/Home/ProfileBox/textMain.js
@@ -1,7 +1,9 @@
 export const textMain = [
-  'My name is Elad and I am looking for a job as a Web / Fullstack / Frontend Developer',
+  'My name is Elad and I am a Web / Fullstack / Frontend Developer',
 
   'I am a self taught and an enthusiastic with great ambitions',
 
   'I believe that communication and social skills working together in a group can have big contribution to the success of the development process',
+  
+  'i\'m currently looking for a new challenge. So if you need a developer like me just contact me'
 ];
